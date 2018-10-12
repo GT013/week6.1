ddacb6605db2181514a21ba18e3965c22d1b4f24
nantikan <60030049@kmitl.ac.th>
2018-10-12 23:35:33 +0700

    add

---
+++ lab6/lab6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object[] myary = {"nantikan",4.00d,13, "April",2542  };
+            testparas(myary);
+            Console.ReadKey();
+        }
+        static void testparas(params object[] detail)
+        {
+            foreach(object t in detail)
+                Console.Write(t + " ");
+            Console.ReadKey();
+        }
+    }
+}
+
