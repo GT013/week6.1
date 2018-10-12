65f9ae22b5381fe60789965011b4ce7aab6e22b6
nantikan <60030049@kmitl.ac.th>
2018-10-13 02:29:23 +0700

    add

--- lab6/lab6/Program.cs
+++ lab6/lab6/Program.cs
@@ -10,15 +10,17 @@ namespace lab6
     {
         static void Main(string[] args)
         {
-            //double GPA = 1.95;
-            //Console.WriteLine("You are " + (GPA >= 2.0 ? "pass" : "fall"));
-            //int x = 10, y = 5;
-            //Console.WriteLine("x > y : " + (x > y));
-            Console.WriteLine("x < y : " + (x < y));
-            Console.WriteLine("x >= y : " + (x >= y));
-            Console.WriteLine("x <= y : " + (x <= y));
-            Console.WriteLine("x == y : " + (x == y));
-            Console.WriteLine("x != y : " + (x != y));
+            bool a = true; // กำหนดให้ a มีค่าเป็น true
+            bool b = false;  // กำหนดให้ a มีค่าเป็น false
+            Console.WriteLine(a && b); // เปรียบเทียบทางตรรกะศาสตร์ a และ b ผลลัพธ์ false
+            Console.WriteLine(a || b); // เปรียบเทียบทางตรรกะศาสตร์ a หรือ b ผลลัพธ์ true
+            Console.WriteLine(!b);  // นิเสธ b ผลลัพธ์ true
+            Console.WriteLine(b || true); // เปรียบเทียบทางตรรกะศาสตร์ b หรือ true ผลลัพธ์ true
+            Console.WriteLine((9 > 7) ^ (a == b)); // 9 มากกว่า 7 จริงได้ true XOR a มีค่าเท่ากับ b เท็จได้ false นำค่า true XOR false ผลลัพธ์ true
+            Console.WriteLine((b != a) || (a == b)); // b ไม่เท่ากับ a จริงได้ true OR a มีค่าเท่ากับ b เท็จได้ false นำค่า true OR false ผลลัพธ์ true
+            Console.WriteLine((b != a) && (a == b)); // b ไม่เท่ากับ a จริงได้ true AND a มีค่าเท่ากับ b เท็จได้ false นำค่า true AND false ผลลัพธ์ false
+            Console.WriteLine((!a) && (!b)); // นิเสธ a AND นิเสธ b // false AND true ผลลัพธ์ false
+            Console.WriteLine((!a) || (!b)); // นิเสธ a OR นิเสธ b // false OR true ผลลัพธ์ true
 
         }
     }
