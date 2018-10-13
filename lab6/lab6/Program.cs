9a50097416a9284a86d5ca4c1554e8ccdde7405f
nantikan <60030049@kmitl.ac.th>
2018-10-13 21:32:33 +0700

    add

--- lab6/lab6/Program.cs
+++ lab6/lab6/Program.cs
@@ -10,20 +10,12 @@ namespace lab6
     {
         static void Main(string[] args)
         {
-            long myLong = long.MaxValue;
-            int myInt = (int)myLong;   // Explicit conversion
-
-            float heightInMeters = 1.74f; // Explicit conversion
-            double maxHeight = heightInMeters; // Implicit
-            double minHeight = (double)heightInMeters; // Explicit
-            float actualHeight = (float)maxHeight; // Explicit
-
-            Console.WriteLine(myLong);
-            Console.WriteLine(myInt);
-            Console.WriteLine(heightInMeters);
-            Console.WriteLine(maxHeight);
-            Console.WriteLine(minHeight);
-            Console.WriteLine(actualHeight);
+            string hello = "Hello";
+            string world = "World";
+            string helloWorld = hello + world;
+            Console.WriteLine(helloWorld);
+            string helloWorld555 = helloWorld + " " + 555;
+            Console.WriteLine(helloWorld555);
 
         }
     }
